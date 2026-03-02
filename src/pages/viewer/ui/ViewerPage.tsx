5b1dbb42a87fb9b6580338fd017169cd70c5abbe
chaegyeom.bae <corua0309@gmail.com>
2026-03-03 05:04:41 +0900

    integrate tailwind css

--- src/pages/viewer/ui/ViewerPage.tsx
+++ src/pages/viewer/ui/ViewerPage.tsx
@@ -1,3 +1,7 @@
 export const ViewerPage = () => {
-  return <div>ViewerPage</div>;
+  return (
+    <div className="bg-slate-900 text-white h-screen flex items-center justify-center">
+      ViewerPage
+    </div>
+  );
 };
